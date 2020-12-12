3461586c08d32e95c0b3893e11d8dec71b039133
dis500 <barca125@yandex.ru>
2020-12-12 19:09:11 +0300

    Finish table resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,136 +1,26 @@
-import { ExcelComponent } from '@core/ExcelComponent';
-import { createTable } from '@/components/table/table.template';
+import { ExcelComponent } from '@core/ExcelComponent'
+import { createTable } from '@/components/table/table.template'
+import {$} from '@core/dom'
+import { resizeHandler } from '@/components/table/table.resize'
+import {shouldResize} from '@/components/table/table.functions'
 
 export class Table extends ExcelComponent {
-	static className = 'excel__table';
+  static className = 'excel__table';
+
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown', 'mousemove']
+    })
+  }
 
 	toHTML() {
-		return createTable();
-
-		 `
-        <div class="row">
-
-        <div class="row-info"></div>
-
-        <div class="row-data">
-          <div class="column">
-            A
-          </div>
-          <div class="column">
-            B
-          </div>
-          <div class="column">
-            C
-          </div>
-          <div class="column">
-            A
-          </div>
-          <div class="column">
-            B
-          </div>
-          <div class="column">
-            C
-          </div>
-          <div class="column">
-            A
-          </div>
-          <div class="column">
-            B
-          </div>
-          <div class="column">
-            C
-          </div>
-          <div class="column">
-            A
-          </div>
-          <div class="column">
-            B
-          </div>
-          <div class="column">
-            C
-          </div>
-          <div class="column">
-            A
-          </div>
-          <div class="column">
-            B
-          </div>
-          <div class="column">
-            C
-          </div>
-          <div class="column">
-            A
-          </div>
-          <div class="column">
-            B
-          </div>
-          <div class="column">
-            C
-          </div>
-          <div class="column">
-            A
-          </div>
-          <div class="column">
-            B
-          </div>
-          <div class="column">
-            C
-          </div>
-          <div class="column">
-            A
-          </div>
-          <div class="column">
-            B
-          </div>
-          <div class="column">
-            C
-          </div>
-          <div class="column">
-            A
-          </div>
-          <div class="column">
-            B
-          </div>
-          <div class="column">
-            C
-          </div>
-          <div class="column">
-            A
-          </div>
-          <div class="column">
-            B
-          </div>
-          <div class="column">
-            C
-          </div>
-
-        </div>
-
-      </div>
-
-      <div class="row">
-        <div class="row-info">
-          1
-        </div>
-
-        <div class="row-data">
-          <div class="cell selected" contenteditable="">A1</div>
-          <div class="cell" contenteditable="">B2</div>
-          <div class="cell" contenteditable="">C3</div>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="row-info">
-          2
-        </div>
-
-        <div class="row-data">
-          <div class="cell">A1</div>
-          <div class="cell">B2</div>
-          <div class="cell">C3</div>
-        </div>
-      </div>
-        ` 
-	}
+		return createTable(20);
+  }
+  
+  onMousedown(event) {
+    if (shouldResize) {
+      resizeHandler(this.$root, event)
+    }
+  }
+  
 }
